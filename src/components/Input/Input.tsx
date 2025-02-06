e3c15fd3213e8ae52f14954fc127dbea78c890ec
KKibirieva <kkibirieva@gmail.com>
2025-02-06 14:58:43 +0200

    upd

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,20 @@
+import { InputContainer, InputElement, Label } from "./styles";
+import { InputProps } from './types'
+
+function Input({ name, type = 'text', placeholder, label, id, value, onChange }: InputProps) {
+  return (
+    <InputContainer>
+      {label && <Label htmlFor={id}>{label}</Label>}
+      <InputElement
+        name={name}
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </InputContainer>
+  );
+}
+
+export default Input;
